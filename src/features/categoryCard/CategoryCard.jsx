c7cbb67d863103a48b15bdef4fe5a0e6c067831d
Arun Shukla <arunshukla98710@gmail.com>
2023-07-29 20:30:14 +0530

    fixed navigation

--- src/features/categoryCard/CategoryCard.jsx
+++ src/features/categoryCard/CategoryCard.jsx
@@ -4,22 +4,23 @@ import { useNavigate } from "react-router-dom";
 import { useData } from "../../context/DataContext";
 
 const CategoryCard = ({ category }) => {
-    const {dispatch} = useData();
-    const navigate = useNavigate()
-    const handleCategoryClick =() =>{
-        
-        dispatch({type:"GET_PARTICULAR_CATEGORY",payload:category})
-        navigate(`/categories/${category.category}`)
-    }
  
+  const { dispatch } = useData();
+  const navigate = useNavigate();
+  const handleCategoryClick = () => {
+    dispatch({ type: "GET_PARTICULAR_CATEGORY", payload: category });
+    console.log(category.category, "navigate");
+    navigate(`/categories/${category.category}`);
+  };
+
   return (
     <div className="categoryCardContainer">
-        <div className="Wholecard" onClick={handleCategoryClick}>
-          <div className="img">
-            <img src={category.thumbnail} alt="thumbnail" />
-          </div>
-          <div className="content">{category.category}</div>
+      <div className="Wholecard" onClick={handleCategoryClick}>
+        <div className="img">
+          <img src={category.thumbnail} alt="thumbnail" />
         </div>
+        <div className="content">{category.category}</div>
+      </div>
     </div>
   );
 };
