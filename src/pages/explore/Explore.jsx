6354f78de195e0dea21e2ae43f4404cef981f38f
Arun Shukla <arunshukla98710@gmail.com>
2023-07-29 20:30:33 +0530

    fixed search bar and rightbar

--- src/pages/explore/Explore.jsx
+++ src/pages/explore/Explore.jsx
@@ -1,24 +1,43 @@
 import React from "react";
 import { useData } from "../../context/DataContext";
 import VideoCard from "../../features/videoCard/VideoCard";
-import "./explore.css"
+import "./explore.css";
 const Explore = () => {
   const {
     state: { videos },
+    dispatch,
+    state,
   } = useData();
 
+  const handleSearchProduct = (e) => {
+    dispatch({ type: "SEARCH_PRODUCT", payload: e.target.value });
+  };
   return (
     <div className="mainCategory">
-        <h1>Explore</h1>
-        <div className="searchBarContainer">
-        <input type="text" className="searchBar" name="search" placeholder="search videos by title" />
+      <h1>Explore</h1>
+      <div className="searchBarContainer">
+        <input
+          type="text"
+          className="searchBar"
+          name="search"
+          placeholder="search videos by title"
+          onChange={handleSearchProduct}
+        />
       </div>
       <div className="categoryContainer">
-      {
-   videos.map((video)=><div className='cardsContainer'>
-       <VideoCard video={video} key={video._id}/>
-   </div>)
-}
+        {videos
+          ?.filter((video) =>
+            state.searchText
+              ? video?.title
+                  .toLowerCase()
+                  .includes(state.searchText.toLowerCase())
+              : true
+          )
+          .map((video) => (
+            <div className="cardsContainer">
+              <VideoCard video={video} key={video._id} />
+            </div>
+          ))}
       </div>
     </div>
   );
